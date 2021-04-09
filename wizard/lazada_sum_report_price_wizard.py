c5b234fe0972dd8dd867de670d6515c6fa6b1d05
Hieu Lam <hieu.lam@ecricode.com>
2021-04-09 07:55:39 +0700

    show bao cao chi phi

--- wizard/lazada_sum_report_price_wizard.py
+++ wizard/lazada_sum_report_price_wizard.py
@@ -12,13 +12,15 @@ import datetime
 import logging
 import re
 import ctypes
+import base64
+import io
 import json
 import pandas as pd
 
 _logger = logging.getLogger(__name__)
 
 
-class LazadaSumAmoutReport(models.Model):
+class LazadaSumAmoutReport(models.TransientModel):
     _name = 'lazada.sum.amount.report.wizard'
     _description = 'Lazada Sum Amount Report Wizard'
 
@@ -38,8 +40,6 @@ class LazadaSumAmoutReport(models.Model):
         so_mgt = self.env['sale.order.management']
         context = self.env.context.copy()
         _li_shop = context.get('default_shop_id')
-        sale_director_file = context.get('sale_director_file')
-        _sale_done_director = context.get('sale_done_director')
         # start_datetime = datetime.datetime.combine(self.date_start,datetime.time.min)
         # end_datetime = datetime.datetime.combine(self.date_end,datetime.time.max)
         start_datetime = self.date_start
@@ -57,65 +57,54 @@ class LazadaSumAmoutReport(models.Model):
         rec_ids = so_mgt.search([
             ('created_at','>=',start_datetime),
             ('created_at','<=',end_datetime),
-            ('state','=','delivered'),
+            # ('state','=','delivered'),
             ('shop_id','in',_li_shop)
         ])
-        self._calculate_lazada_price_product(rec_ids,sale_director_file,_sale_done_director)
-        result = rec_ids.filtered(lambda r: r.state == 'delivered')
-        return {
-            'name': _('Đối Soát Đơn Hàng'),
-            'view_type': 'form',
-            'view_mode': 'tree',
-            'view_id': False,
-            'res_model': 'sale.order.management',
-            'context': {},
-            'domain': [('id','in',result.ids)],
-            'target': 'main',
-            'type': 'ir.actions.act_window',
-        }
+        print('rec_ids: ',rec_ids)
+        return self._calculate_lazada_price_product(rec_ids)
     
-    def _calculate_lazada_price_product(self, rec_ids, sale_director_file, _sale_done_director):
+    def _calculate_lazada_price_product(self, rec_ids):
         create_data = {}
-        for entry in sale_director_file:
-            directory = "{}/{}".format(_sale_done_director,entry)
-            result = pd.read_csv(directory,sep=',',encoding='utf8')
-            for index, row in result.iterrows():
-                fee_name = row[FEE_NAME].strip()
-                if fee_name == MARKETING_SOLUTION_SOCIAL_MEDIA_ADVERTISING:
-                    continue
-                order_id = int(row[ODER_ITEM_NO])
-                rec = rec_ids.filtered(lambda r: r.order_item_id == str(order_id))
-                if not rec.id:
-                    continue
-                order_no = str(int(row[ORDER_NO]))
-                order_dict = create_data.get(order_no,False)
-                if not order_dict:
-                    order_dict = {}
-                item_dict = order_dict.get(order_id,False)
-                if not item_dict:
-                    item_dict = {
-                        PAYMENT_FEE: 0.0,
-                        SHIPPING_FEE_PAID_BY_CUSTOMER: 0.0,
-                        ITEM_PRICE_CREDIT: 0.0,
-                        PROMOTIONAL_CHARGES_VOUCHERS: 0.0,
-                        SHIPPING_FEE_VOUCHER_BY_LAZADA: 0.0,
-                        SHIPPING_FEE_PAID_BY_SELLER: 0.0,
-                        PROMOTIONAL_CHARGES_FLEXICOMBO: 0.0,
-                        REVERSAL_ITEM_PRICE: 0.0,
-                        REVERSAL_SHIPPING_FEE_PAID_BY_CUSTOMER: 0.0,
-                        REVERSAL_PROMOTIONAL_CHARGES_FLEXICOMBO: 0.0,
-                        REVERSAL_SHIPPING_FEE_VOUCHER_BY_LAZADA: 0.0,
-                        REVERSAL_PROMOTIONAL_CHARGES_VOUCHERS: 0.0,
-                        LAZADA_BONUS: 0.0,
-                        LAZADA_BONUS_LZD_COFUND: 0.0,
-                        SPONSORED_DISCOVERY_TOP_UP: 0.0
-                    }
-                _amount = item_dict.get(fee_name,0.0)
-                item_dict.update({
-                    fee_name: _amount + row[AMOUNT],
-                })
-                order_dict.update({order_id: item_dict})
-                create_data.update({order_no: order_dict})
+        data_file = base64.b64decode(self.file_data)
+        csv_filelike = io.BytesIO(data_file)
+        result = pd.read_csv(csv_filelike,sep=',',encoding='utf8', usecols=[FEE_NAME, AMOUNT, ORDER_NO, ORDER_STATUS,ODER_ITEM_NO], dtype={ORDER_NO: int})
+        for index, row in result.iterrows():
+            fee_name = row[FEE_NAME].strip()
+            if fee_name == MARKETING_SOLUTION_SOCIAL_MEDIA_ADVERTISING:
+                continue
+            order_id = int(row[ODER_ITEM_NO])
+            rec = rec_ids.filtered(lambda r: r.order_item_id == str(order_id))
+            if not rec.id:
+                continue
+            order_no = str(int(row[ORDER_NO]))
+            order_dict = create_data.get(order_no,False)
+            if not order_dict:
+                order_dict = {}
+            item_dict = order_dict.get(order_id,False)
+            if not item_dict:
+                item_dict = {
+                    PAYMENT_FEE: 0.0,
+                    SHIPPING_FEE_PAID_BY_CUSTOMER: 0.0,
+                    ITEM_PRICE_CREDIT: 0.0,
+                    PROMOTIONAL_CHARGES_VOUCHERS: 0.0,
+                    SHIPPING_FEE_VOUCHER_BY_LAZADA: 0.0,
+                    SHIPPING_FEE_PAID_BY_SELLER: 0.0,
+                    PROMOTIONAL_CHARGES_FLEXICOMBO: 0.0,
+                    REVERSAL_ITEM_PRICE: 0.0,
+                    REVERSAL_SHIPPING_FEE_PAID_BY_CUSTOMER: 0.0,
+                    REVERSAL_PROMOTIONAL_CHARGES_FLEXICOMBO: 0.0,
+                    REVERSAL_SHIPPING_FEE_VOUCHER_BY_LAZADA: 0.0,
+                    REVERSAL_PROMOTIONAL_CHARGES_VOUCHERS: 0.0,
+                    LAZADA_BONUS: 0.0,
+                    LAZADA_BONUS_LZD_COFUND: 0.0,
+                    SPONSORED_DISCOVERY_TOP_UP: 0.0
+                }
+            _amount = item_dict.get(fee_name,0.0)
+            item_dict.update({
+                fee_name: _amount + row[AMOUNT],
+            })
+            order_dict.update({order_id: item_dict})
+            create_data.update({order_no: order_dict})
         csv_data = self._create_lzd_sum(data=create_data)
         csv_file = self._create_csv_file(csv_data=csv_data)
         vals = {
@@ -156,8 +145,8 @@ class LazadaSumAmoutReport(models.Model):
         
         order_no = order_item_no = payment_fee = shipping_fee_paid_by_customer = item_price_credit = promotional_charges_vouchers = shipping_fee_voucher_by_lazada = shipping_fee_paid_by_seller = promotional_charges_flexi_combo = reversal_item_price = reversal_shipping_fee_by_customer = reversal_promotional_charges_flexi_combo = reversal_shipping_fee_voucher_lazada = reversal_promotional_charges_vouchers = lazada_bouns = lazada_bouns_lzd_co_fund = sponsored_discoverty_top_up = []
         for item in csv_data:
-            order_no.append(item[order_number])
-            order_item_no.append(item[order_item_id])
+            order_no.append(item['order_number'])
+            order_item_no.append(item['order_item_id'])
             payment_fee.append(item[PAYMENT_FEE])
             shipping_fee_paid_by_customer.append(item[SHIPPING_FEE_PAID_BY_CUSTOMER])
             item_price_credit.append(item[ITEM_PRICE_CREDIT])
@@ -174,7 +163,7 @@ class LazadaSumAmoutReport(models.Model):
             lazada_bouns_lzd_co_fund.append(item[LAZADA_BONUS_LZD_COFUND])
             sponsored_discoverty_top_up.append(item[SPONSORED_DISCOVERY_TOP_UP])
 
-        df = dp.DataFrame({
+        df = pd.DataFrame({
             ORDER_NO: order_no,
             ODER_ITEM_NO: order_item_no,
             PAYMENT_FEE: payment_fee,
